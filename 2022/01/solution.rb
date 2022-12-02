75184d568aa631d8f581ae775994dfd471a616e2
Netmentor <ivanxo69@gmail.com>
2022-12-02 08:23:35 +0000

    updpate to oneliner

--- 2022/01/solution.rb
+++ 2022/01/solution.rb
@@ -1,18 +1,10 @@
-file = File.open("input.txt")
+file = File.open('input.txt')
 elves_data = file.read
 
-elves = elves_data.split("\n\n")
+elves_with_calories = elves_data.split("\n\n").map { |elf| elf.split("\n").map(&:to_i).sum }
 
-elves_with_calories = [] 
-
-elves.each do | elve |
-    calories = elve.split("\n").each.map(&:to_i)
-    elves_with_calories << calories.sum
-end
-
-
-puts "the most performer:"
+puts 'the most performer:'
 puts elves_with_calories.max
 
-puts "the Top 3 performers:"
+puts 'the Top 3 performers:'
 puts elves_with_calories.max(3).sum
